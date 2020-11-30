c246d30d2e2345590e8712a4bafd4ded27a28830
bryanmoon1991 <bryanmoon1991@gmail.com>
2020-11-30 11:06:01 -0500

    changed haiku card css slightly

--- src/Components/HaikuCard.js
+++ src/Components/HaikuCard.js
@@ -3,6 +3,9 @@ import './HaikuCard.css'
 import { Link } from 'react-router-dom'
 const HaikuCard = ({ haiku, getProfile }) => {
 
+
+    
+
     return (
       <div className="haiku-card">
         <img src={haiku.user.image} alt="profile Pic" />
